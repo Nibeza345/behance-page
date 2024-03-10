1bf125ab161d5c85d7c40d1de3e772eec5634eaa
NIBEZA345 <nicole.nibeza345@gmail.com>
2024-03-10 21:34:26 +0200

    updated

---
+++ backend/edit.php
@@ -0,0 +1,31 @@
+<?php
+// Establish database connection
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+header('Content-type: application/json');
+// Get signup data
+$username = $_POST['edit-name'];
+$password = $_POST['edit-email'];
+$email = $_POST['edit-password'];
+
+// Insert the user into the database
+$sql = "INSERT INTO project1(username, email, password) VALUES('$username', '$email', '$password')";
+$result= mysqli_query($conn , $sql);
+
+if ($result) {
+  echo json_encode("successful");
+} else {
+    echo  json_encode("failed");
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
--- js/users.js
+++ js/users.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
                     <td> <input type="checkbox" id="delete"> </td>
                     <td> <button type="button" id="edit" onclick="edit(${user.id})" data-id=${user.id}>Edit</button></td>
                     `
-                    tbody.appendChild(tr)           
+                    tbody.appendChild(tr)
                 })
             },
             error: function (error) {
@@ -28,26 +28,53 @@ $(document).ready(function () {
         });
     })
 })
-
+// assigning the value in the current user info into the edit form
 function edit(id) {
-    console.log("called with id : ",id)
     $.ajax({
         url: '../backend/get_users.php',
         method: 'GET',
         dataType: 'json',
         success: function (response) {
             $.each(response, (index, user) => {
-              if (user.id == id) {
-                var name = document.getElementById("edit-name");
-                name.value = user.username
-                var email = document.getElementById("edit-email");
-                var password = document.getElementById("edit-password");
-              }
+                if (user.id == id) {
+                    var name = document.getElementById("edit-name");
+                    name.value = user.username
+                    var email = document.getElementById("edit-email");
+                    email.value = user.email
+                    var password = document.getElementById("edit-password");
+                    password.value = user.password
+                }
             })
 
         },
         error: function (error) {
             console.log("failed", error);
         }
-    });
-}
+    }
+    
+    );
+    
+        $('#save').click(function (e) {
+            e.preventDefault();
+            console.log("form submitted!")
+            var username = $('#edit-name').val();
+            var email = $('#edit-email').val();
+            var password = $('#edit-password').val();
+    
+            // Send the signup data to the server
+            $.ajax({
+                url: '../backend/edit.php',
+                method: 'POST',
+                data: {
+                    username: username,
+                    password: password,
+                    email: email
+                },
+                success: function (response) {
+                    console.log(response);
+                    
+                }
+            });
+    
+        });
+    }
